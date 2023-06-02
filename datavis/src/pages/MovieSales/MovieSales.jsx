20ada07a808fa8fbdf9d6101b07895bb05e878b0
richardleilei <114229834+richardleilei@users.noreply.github.com>
2023-06-02 14:24:15 +0200

    Update MovieSales.jsx
    adding bubble chart

--- datavis/src/pages/MovieSales/MovieSales.jsx
+++ datavis/src/pages/MovieSales/MovieSales.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
 import {
   StyledInfo,
@@ -11,6 +11,19 @@ import {
 import logo from "../../assets/main_logo.png";
 
 const MovieSales = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src = "https://public.flourish.studio/resources/embed.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <StyledInfo>
       <StyledInfoSection>
@@ -19,12 +32,14 @@ const MovieSales = () => {
         </StyledInfoHeader>
         <StyledInfoMain>
           <p>
-            Description
+            High rating, or high box office, that is the question. This bubble chart shows the relationship between the two.
           </p>
+          <div
+            className="flourish-embed flourish-scatter"
+            data-src="visualisation/13980087"
+          />
         </StyledInfoMain>
       </StyledInfoSection>
-
-      
     </StyledInfo>
   );
 };
